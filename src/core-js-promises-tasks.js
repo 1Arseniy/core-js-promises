6bc441d6e420f82137aceec2cebf58f1522797f6
Arseniy <krutoi.arseniy@gmail.com>
2025-03-24 10:36:19 +0300

    feat: solve finish problem

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -149,10 +149,17 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  let str = '';
+  return promises.reduce((acc, cur) => {
+    return acc.then(() => {
+      return cur.then((value) => {
+        str += value;
+        return str;
+      });
+    });
+  }, Promise.resolve());
 }
-
 module.exports = {
   getPromise,
   getPromiseResult,
